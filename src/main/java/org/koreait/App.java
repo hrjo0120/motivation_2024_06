5a55adf3624126a4ad68959d7f845b1c96426a30
hrjo0120 <hrjo0120@gmail.com>
2024-06-28 10:39:49 +0900

    번호 증가 변경, list 명령어 생성

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class App {
     private Scanner sc;
-    private int id;
 
     public App(Scanner sc) {   //생성자 = 객체 생성될때 자동실행되는 메소드
         this.sc = sc;
@@ -13,10 +12,12 @@ public class App {
 
     public void run() {
         System.out.println("== motivation 실행 ==");
+
+        int lastId = 1;
+
         while (true) {
             System.out.print("명령어) ");
             String cmd = sc.nextLine().trim();
-
 
             if (cmd.equals("exit")) {
                 System.out.println("== motavation 종료 ==");
@@ -28,12 +29,18 @@ public class App {
 
             // 기능
             if (cmd.equals("add")) {
-                id++;
-                System.out.print("motivation : ");
-                String motivation = sc.nextLine();
+                int id = lastId + 1;
+                System.out.print("body : ");
+                String body = sc.nextLine();
                 System.out.print("source : ");
                 String source = sc.nextLine();
-                System.out.printf("%d번 motivation이 등록 되었습니다.\n", this.id);
+                System.out.printf("%d번 motivation이 등록 되었습니다\n", lastId);
+                lastId++;
+            } else if (cmd.equals("list")) {
+                System.out.println("== motivation list ==");
+                System.out.println("  id  //   motivation   //  source");
+                System.out.println("=".repeat(35));
+
             }
         }
 
