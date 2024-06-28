5699789d4ee17fa83752b69125572369e46d4a85
hrjo0120 <hrjo0120@gmail.com>
2024-06-28 16:49:13 +0900

    명령어 parsing

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -4,8 +4,7 @@ import org.koreait.motivation.controller.MotivationController;
 import org.koreait.motivation.entity.Motivation;
 import org.koreait.system.controller.SystemController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class App {
 
@@ -14,7 +13,6 @@ public class App {
 
         SystemController systemController = new SystemController();
         MotivationController motivationController = new MotivationController();
-
 
 
         int lastId = 0; // 몇 번까지 썼더라?
@@ -37,6 +35,33 @@ public class App {
                 motivationController.add();
             } else if (cmd.equals("list")) {
                 motivationController.list();
+            } else if (cmd.startsWith("delete")) {
+                motivationController.delete(cmd);
+                // parsing
+
+                String[] cmdBits = cmd.split("\\?", 2);
+
+                String actionMethod = cmdBits[0];   // delete
+                Map<String, String> params = new HashMap<>();
+                String[] paramBits = cmdBits[1].split("&");
+
+                for (String paramStr : paramBits) {
+                    String[] paramStrBits = paramStr.split("=", 2);
+                    String key = paramStrBits[0];
+                    String value = paramStrBits[1];
+                    System.out.println("paramStr: " + paramStr + " key: " + key + " value: " + value);
+                    params.put(key, value);
+                }
+
+                System.out.println("Arrays.toString(cmdBits) : " + Arrays.toString(cmdBits));
+                System.out.println("actionMethod: " + actionMethod);
+                System.out.println("params: " + params);
+                System.out.println("paramBits: " + Arrays.toString(paramBits));
+
+//                motivationController.delete();
+
+            } else {
+                System.out.println("사용할 수 없는 명령어입니다.");
             }
         }
     }
