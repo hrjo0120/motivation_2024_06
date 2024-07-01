420f66423e9a452c6c86959591a8aad5d86729c9
hrjo0120 <hrjo0120@gmail.com>
2024-07-01 10:34:08 +0900

    Rq 파싱을 모든 명령어에 도입, 라우팅 코드 간결화

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,12 +1,10 @@
 package org.koreait;
 
 import org.koreait.motivation.controller.MotivationController;
-import org.koreait.motivation.entity.Motivation;
 import org.koreait.system.controller.SystemController;
 
-import java.util.*;
-
 public class App {
+    byte system_status = 1;
 
     public void run() {
         System.out.println("== motivation execution ==");
@@ -14,41 +12,36 @@ public class App {
         SystemController systemController = new SystemController();
         MotivationController motivationController = new MotivationController();
 
-
-        int lastId = 0; // 몇 번까지 썼더라?
-        List<Motivation> motivations = new ArrayList<>(); // motivation 저장소
-
-        while (true) {
+        while (system_status == 1) {
             System.out.print("command) ");
             String cmd = Container.getScanner().nextLine().trim();
 
-            if (cmd.equals("exit")) {
-                systemController.exit();
-                break;
-            } else if (cmd.length() == 0) {
-                System.out.println("명령어를 입력해주세요.");
+            if (cmd.length() == 0) {
+                System.out.println("명령어 입력해주세요");
                 continue;
             }
 
+            Rq rq = new Rq(cmd);
+
             // 기능
-            if (cmd.equals("add")) {
-                motivationController.add();
-            } else if (cmd.equals("list")) {
-                motivationController.list();
-            } else if (cmd.startsWith("delete")) {
-
-                Rq rq = new Rq(cmd);
-
-                System.out.println(rq.getActionMethod());
-                System.out.println(rq.getParams("id"));
-                System.out.println(rq.getParams("source"));
-                System.out.println(rq.getParams("motivation"));
-
-//                motivationController.delete();
-
-            } else {
-                System.out.println("사용할 수 없는 명령어입니다.");
+            switch (rq.getActionMethod()) {
+                case "exit":
+                    systemController.exit();
+                    system_status = 0;
+                    break;
+                case "add":
+                    motivationController.add();
+                    break;
+                case "list":
+                    motivationController.list();
+                    break;
+                case "delete":
+//                    motivationController.delete();
+                default:
+                    System.out.println("사용할 수 없는 명령어입니다");
+                    break;
             }
+
         }
     }
 }
